a16f29e3fc3d645fb216103ad52831071317dd68
Erin Molloy <ekmolloy@cbcb03.umiacs.umd.edu>
2024-01-03 00:42:18 -0500

    Copy some files for convenience

---
+++ han2024wtreeqmc/scripts/mirarab2015astral2-extsim/e1_copy_files.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+NTAX=$1
+HGHT=$2
+RATE=$3
+REPL=$4
+NGEN=$5
+
+MODL="model.$NTAX.$HGHT.$RATE"
+MYMODL="$NTAX,$HGHT,$RATE,$REPL,NA,$NGEN"
+
+# Define directories and files
+LABDIR="/fs/cbcb-lab/ekmolloy"
+PROJDIR="$LABDIR/ekmolloy/tree-qmc-study/han2024wtreeqmc"
+
+# Define software files
+COMPARE="$PROJDIR/tools/compare_two_trees.py"
+
+# Define input files
+INDIR="$LABDIR/group/data/mirarab2015astral2/$MODL/$REPL"
+STRE="s_tree.trees"
+GTRE="estimatedgenetre"
+CAML="concatenatedtree.genes${NGEN}" 
+
+# Define output directory
+OUTDIR="$PROJDIR/data/mirarab2015astral2-extsim/$MODL/$REPL"
+cd $OUTDIR
+
+cp $INDIR/$STRE $STRE
+cp $INDIR/$GTRE $GTRE
+
+if [ -e $INDIR/$CAML ]; then
+    MYMTHD="caml_${NGEN}gen"
+    cp $INDIR/$CAML $MYMTHD.tre
+    MYERRR=$(python3 $COMPARE -t1 $STRE -t2 $MYMTHD.tre)
+    echo "$MYMODL,$MYMTHD,$MYERRR" > ${MYMTHD}_species_tree_error.csv
+fi
+
---
+++ han2024wtreeqmc/scripts/mirarab2015astral2-extsim/e1_run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+exit
+
+# ILS STUDY PARAMETERS
+NTAXS=( 200 )                            # Number of taxa
+HGHTS=( "10000000" "2000000" "500000" )  # Species tree height (number of generations)
+RATES=( "0.0000001" "0.000001" )         # Speciation rate
+
+# SCALABILITY STUDY PARAMETERS
+NTAXS=( 10 50 100 500 1000 )      # Number of taxa
+HGHTS=( "2000000" )               # Species tree height (number of generations)
+RATES=( "0.000001" )              # Speciation rate
+
+# GENERAL PARAMETERS
+REPLS=( $(seq -f "%02g" 1 50) )   # Replicates
+NGENS=( 50 200 1000 )
+
+for NTAX in ${NTAXS[@]}; do
+    for HGHT in ${HGHTS[@]}; do
+        for RATE in ${RATES[@]}; do
+            MODL="model.$NTAX.$HGHT.$RATE"
+            for REPL in ${REPLS[@]}; do
+                for NGEN in ${NGENS[@]}; do
+echo "Submitting $MODL $REPL $NGEN ..."
+bash e1_copy_files.sh $NTAX $HGHT $RATE $REPL $NGEN
+                done
+            done
+        done
+    done
+done
+
