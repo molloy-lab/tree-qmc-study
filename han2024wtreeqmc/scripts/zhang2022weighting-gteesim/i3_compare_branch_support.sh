4675f7a59bc926c86124cdaa5178050c4af89edf
Erin Molloy <ekmolloy@nexuscbcb00.umiacs.umd.edu>
2024-01-10 01:01:10 -0500

    streamlined processing

--- han2024wtreeqmc/scripts/zhang2022weighting-gteesim/i3_compare_branch_support.sh
+++ han2024wtreeqmc/scripts/zhang2022weighting-gteesim/i3_compare_branch_support.sh
@@ -12,7 +12,7 @@ GROUPDIR="/fs/cbcb-lab/ekmolloy"
 PROJDIR="$GROUPDIR/ekmolloy/tree-qmc-study/han2024wtreeqmc"
 
 # Define software
-COMPARE="$PROJDIR/tools/compare_support_for_false_branches.py"
+COMPARE="$PROJDIR/tools/compare_branch_support.py"
 
 # Define input files
 DATADIR="$PROJDIR/data/zhang2022weighting-gteesim/S100/$REPL"
@@ -26,7 +26,7 @@ for QSUP in "qsupp-wh" "qsupp-wn"; do
     STRE_TRUE="$DATADIR/true_stree_${SUPP}_${NBPS}bps_${NGEN}gen_${QSUP}.tre"
     STRE_EST1="$DATADIR/${MTHD1}_${SUPP}_${NBPS}bps_${NGEN}gen_${QSUP}.tre"
     STRE_EST2="$DATADIR/${MTHD2}_${SUPP}_${NBPS}bps_${NGEN}gen_${QSUP}.tre"
-    CSVF="$DATADIR/false_branches_${MTHD1}_vs_${MTHD2}_for_${SUPP}_${NBPS}bps_${NGEN}gen_${QSUP}.csv"
+    CSVF="$DATADIR/branch_support_${MTHD1}_vs_${MTHD2}_for_${SUPP}_${NBPS}bps_${NGEN}gen_${QSUP}.csv"
 
     for TREE in $STRE_TRUE $STRE_EST1 $STRE_EST2; do
         if [ ! -e $TREE ]; then
