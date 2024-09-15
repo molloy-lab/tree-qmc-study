3cf158e0f75d44fc33811f838596b5ffa91a5189
Erin Molloy <ekmolloy@nexuscbcb00.umiacs.umd.edu>
2024-09-15 00:16:29 -0400

    Updating scripts

--- han2024wtreeqmc/scripts/mirarab2015astral2-extsim/j1_compare_branch_support.sh
+++ han2024wtreeqmc/scripts/mirarab2015astral2-extsim/j1_compare_branch_support.sh
@@ -15,7 +15,7 @@ GROUPDIR="/fs/cbcb-lab/ekmolloy"
 PROJDIR="$GROUPDIR/ekmolloy/tree-qmc-study/han2024wtreeqmc"
 
 # Define software
-COMPARE="$PROJDIR/tools/compare_support_for_false_branches.py"
+COMPARE="$PROJDIR/tools/compare_branch_support.py"
 
 # Define input files
 DATADIR="$PROJDIR/data/mirarab2015astral2-extsim/$MODL/$REPL"
@@ -23,24 +23,26 @@ DATADIR="$PROJDIR/data/mirarab2015astral2-extsim/$MODL/$REPL"
 MTHD1="aster_h_t16"
 MTHD2="wtreeqmc_wh_n2"
 
-STRE_TRUE="$DATADIR/true_stree_${SUPP}_${NGEN}gen-ah-annotated.tre"
-STRE_EST1="$DATADIR/${MTHD1}_${SUPP}_${NGEN}gen-ah-annotated.tre"
-STRE_EST2="$DATADIR/${MTHD2}_${SUPP}_${NGEN}gen-ah-annotated.tre"
-OUTF="$DATADIR/false_branches_${MTHD1}_vs_${MTHD2}_for_${SUPP}_${NGEN}gen-ah-annotated.csv"
+for QSUP in "qsupp-wh" "qsupp-wn"; do
+    STRE_TRUE="$DATADIR/true_stree_${SUPP}_${NGEN}gen_${QSUP}.tre"
+    STRE_EST1="$DATADIR/${MTHD1}_${SUPP}_${NGEN}gen_${QSUP}.tre"
+    STRE_EST2="$DATADIR/${MTHD2}_${SUPP}_${NGEN}gen_${QSUP}.tre"
+    OUTF="$DATADIR/branch_support_${MTHD1}_vs_${MTHD2}_for_${SUPP}_${NGEN}gen_${QSUP}.csv"
 
-for TREE in $STRE_TRUE $STRE_EST1 $STRE_EST2; do
-    if [ ! -e $TREE ]; then
-        echo "$TREE is missing!"
-	exit
-    fi
+    for TREE in $STRE_TRUE $STRE_EST1 $STRE_EST2; do
+        if [ ! -e $TREE ]; then
+            echo "$TREE is missing!"
+	    exit
+        fi
 
-    if [ -z $(grep ";" $TREE) ]; then
-        echo "$TREE is empty!"
-	exit
+        if [ -z $(grep ";" $TREE) ]; then
+            echo "$TREE is empty!"
+	    exit
+        fi
+    done
+
+    if [ ! -e $OUTF ]; then
+        python3 $COMPARE -t $STRE_TRUE -e1 $STRE_EST1 -e2 $STRE_EST2 -p $MYMODL &> $OUTF
     fi
 done
-
-if [ ! -e $OUTF ]; then
-    python3 $COMPARE -t $STRE_TRUE -e1 $STRE_EST1 -e2 $STRE_EST2 -p $MYMODL &> $OUTF
-fi
 
